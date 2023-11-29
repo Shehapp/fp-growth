37f98525d94e06d4787aff42deea601703c598c3
shehap <shehap@gmail.com>
2023-11-30 00:26:58 +0200

    add sort operator in struct freq

--- yeah its me/Preprocessing.h
+++ yeah its me/Preprocessing.h
@@ -5,17 +5,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void set_support(int min_support);
 struct freq{
     string item;
     string frequent;
+
+    bool operator<(const freq& other) const {
+        return frequent < other.frequent;
+        // You can customize the comparison based on your needs
+    }
 };
 
 vector<vector<string>> read_input();
 vector<freq> get_c1_frequent(
-            vector<vector<string>> transactions
-    );
+            const vector<vector<string>>& transactions);
 vector<vector<string>> rebuild_transactions(
-        vector<vector<string>> transactions,
+        const vector<vector<string>>& transactions,
         const vector<freq>& c1_frequent
 );
 
